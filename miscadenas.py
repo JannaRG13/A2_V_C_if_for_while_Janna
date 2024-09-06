0a120492bc5a890f0d8e832740d9bb987059ae00
Janna Ramirez Granados <a.22308051281287@cbtis128.edu.mx>
2024-09-06 09:35:05 -0600

    Add files via upload
    Adjunté dos archivos

---
+++ miscadenas.py
@@ -0,0 +1,19 @@
+nombre="Janna"
+print(nombre[0])
+print(nombre)
+
+MisNovias=["J","a","n"]
+print(" -- OTRO EJEMPLO -- ")
+for chicas in MisNovias:
+    print(chicas)
+    b = "Janna Ramirez"
+print(b[2:5])
+
+print(" -- A MAYUSCULAS -- ")
+a = "janna ramirez"
+print(a.upper())
+print(" -- CONCATENAR -- ")
+a = "Janna"
+b = "Ramirez"
+c = a + "_" +b
+print(c)
---
+++ mivariables.py
@@ -0,0 +1,16 @@
+import math
+edad=17 # 65 se guarda en edad
+Nombre="Roberto Gaytan"
+print("Tu edad es ",edad)
+print("Te llamas",Nombre)
+
+print("--- Tipos de variables ---")
+x = str(7)    # x will be '3'
+y = int(3.14159)    # y will be 3
+z = float(7)  # z will be 3.0
+print(x)
+print(y)
+print(z)
+print("---FUNCIONES MATEMATICAS---")
+x=math.sqrt(64)
+print(x)
